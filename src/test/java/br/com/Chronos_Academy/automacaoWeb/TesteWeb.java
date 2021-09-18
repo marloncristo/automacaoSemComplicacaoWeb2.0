0b208bbf7e8468d7a9f3ea16ecac8221885ac189
marloncristo <marlon.cristo.paula@gmail.com>
2021-09-18 12:38:57 -0300

    Anotacoes Junit

--- src/test/java/br/com/Chronos_Academy/automacaoWeb/TesteWeb.java
+++ src/test/java/br/com/Chronos_Academy/automacaoWeb/TesteWeb.java
@@ -1,37 +1,51 @@
 package br.com.Chronos_Academy.automacaoWeb;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteWeb {
+    // criando o objeto driver
+    ChromeDriver driver;
+
+    @Before
+    public void inicializaTeste(){
+    // configura o driver para o navegador Chrome
+     WebDriverManager.chromedriver().setup();
+
+     //instancia da classe ChromeDriver para usar os metodos da classe
+     driver = new ChromeDriver();
+     driver.manage().window().maximize();
+
+     //acessa uma página
+     driver.get("https://www.chronosacademy.com.br");
+    }
+
     @Test
     public void primeiroTeste(){
-        // configura o driver para o navegador Chrome
-        WebDriverManager.chromedriver().setup();
-        // criando o objeto driver
-        ChromeDriver driver;
+    // localizando elemento
+    String xpathTitulo = "//section[2]//h4";
+    WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
+    //recuperando o texto do elemento
+    String textoElementoTxtTitulo = txtTitulo.getText();
 
-        //instancia da classe ChromeDriver para usar os metodos da classe
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        //acessa uma página
-        driver.get("https://www.chronosacademy.com.br");
+    //compara o texto do elemento com o esperado
+    Assert.assertEquals("Porque Tempo É Conhecimento",textoElementoTxtTitulo);
+    }
 
-        // localizando elemento
-        String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
-        WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
-        //recuperando o texto do elemento
-        String textoElementoTxtTitulo = txtTitulo.getText();
+    @Test
+    public void segundoTeste(){
+        System.out.println("Teste dois");
+    }
 
-        //compara o texto do elemento com o esperado
-        Assert.assertEquals("Porque Tempo É Conhecimento",textoElementoTxtTitulo);
-
+    @After
+    public void finalizaTeste(){
         driver.quit();
-
     }
 
 }
